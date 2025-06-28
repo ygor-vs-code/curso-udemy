fa3e760c51e0a9c6ded775bacd0234e6ce2ba495
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-06-28 20:53:25 -0300

    aula 3 e 4

---
+++ python-luiz-otavio/curso_python/aula3.py
@@ -0,0 +1,29 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+print('----------')
+
+# Aspas simples
+
+print('Olá, mundo!')
+print('Olá, "mundo"!')
+print('--------------')
+# Aspas duplas
+
+print("Olá, mundo!")
+print("Olá, 'mundo'!")
+print("--------------")
+
+# Escape
+
+print("Olá, \"mundo\"!")
+print("--------------")
+
+# r
+print(r"Olá, \"mundo\"!")
+print("--------------")
