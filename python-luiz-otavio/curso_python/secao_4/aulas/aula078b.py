e6a2845b0c74093b6356f104a28a5ce37478afd4
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-07-17 17:02:24 -0300

    ex080

---
+++ python-luiz-otavio/curso_python/secao_4/aulas/aula078b.py
@@ -0,0 +1,20 @@
+# Métodos úteis:
+# add, update, clear, discard
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá mundo')
+
+print(s1)
+
+
+
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+
---
+++ python-luiz-otavio/curso_python/secao_4/aulas/aula080.py
@@ -0,0 +1,12 @@
+"""
+Exercício
+Crie uma função que encontra o primeiro duplicado considerando o segundo
+número como a duplicação. Retorne a duplicação considerada.
+Requisitos:
+    A ordem do número duplicado é considerada a partir da segunda
+    ocorrência do número, ou seja, o número duplicado em si.
+    Exemplo:
+        [1, 2, 3, 3, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
+        [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+    Se não encontrar duplicados na lista, retorne - 1
+"""
