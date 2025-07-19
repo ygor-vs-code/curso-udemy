ccc15eb3990ab91e0aff813764a4cac288e3a0c1
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-07-19 20:13:55 -0300

    aula083

---
+++ python-luiz-otavio/curso_python/secao_4/aulas/aula083.py
@@ -0,0 +1,13 @@
+a, b = 1, 2
+a, b = b, a
+print(a, b)
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza'
+}
+
+# args e kwargs
+# args (já vimos)
+# kwargs - keyword argumentos (argumentos nomeados)
+
