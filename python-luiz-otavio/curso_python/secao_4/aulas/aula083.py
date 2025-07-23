2d6bfbf62123660e37abe46705a64b0be6f1d0b1
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-07-23 13:21:01 -0300

    aula084

--- python-luiz-otavio/curso_python/secao_4/aulas/aula083.py
+++ python-luiz-otavio/curso_python/secao_4/aulas/aula083.py
@@ -1,13 +1,44 @@
 a, b = 1, 2
 a, b = b, a
-print(a, b)
+# print(a, b)
 
 pessoa = {
     'nome': 'Aline',
     'sobrenome': 'Souza'
 }
 
+dados_pessoa = {
+    'idade': 16,
+    'alutra': 1.6
+}
+
+pessoa_completa = {**pessoa, **dados_pessoa}
+# print(pessoa_completa)
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
 # args e kwargs
 # args (já vimos)
 # kwargs - keyword argumentos (argumentos nomeados)
 
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('Não nomeados:', args)
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+# mostro_argumentos_nomeados(1, 2, nome='joana', qlq=123)
+# mostro_argumentos_nomeados(**pessoa_completa)
+
+configs = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+
+mostro_argumentos_nomeados(**configs)
