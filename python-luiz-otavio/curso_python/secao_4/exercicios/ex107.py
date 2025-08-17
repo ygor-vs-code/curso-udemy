6bce3d9584208cfa7490a62ff16a2ecc561bd591
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-08-17 17:37:41 -0300

    aula107

---
+++ python-luiz-otavio/curso_python/secao_4/exercicios/ex107.py
@@ -0,0 +1,22 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+lista_1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+lista_2 = ['BA', 'SP', 'MG', 'RJ']
+lista_geral = set()
+
+if len(lista_1) < len(lista_2):
+    valores = []
+    for item1 in lista_1:
+        for item2 in lista_2:
+            lista_geral.add((item1, item2))
+
+print(lista_geral, sep='\n')
