7350e6989824bb61a10e831136a3a5f5db0dad92
ygor-vs-code <ygor.vs.code@gmail.com>
2026-07-12 09:03:27 -0300

    ex127

---
+++ python-luiz-otavio/curso_python/secao_5/exercicios/ex127.py
@@ -0,0 +1,25 @@
+import json
+
+CAMINHO_ARQUIVO = "python-luiz-otavio/curso_python/secao_5/exercicios/ex127.json"
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+p1 = Pessoa("João", 33)
+p2 = Pessoa("Helena", 21)
+p3 = Pessoa("Joana", 11)
+bd = [vars(p1), p2.__dict__, vars(p3)]
+
+def fazer_dump():
+    with open(CAMINHO_ARQUIVO, "w") as arquivo:
+        print("Fazendo Dump")
+        json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    print("ELE É O __main__")
+    fazer_dump()
